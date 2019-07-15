2e012552bc9f44afe28b60a806697ae4edb9eaf8
Austinskas <a.austinskas@yahoo.com>
2019-07-15 21:39:46 +0300

    first

---
+++ src/app/components/Title/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./index.scss";
+
+function Title({
+  children,
+  level = 1,
+  colorClass = "color-yellow",
+  weightClass = "font-weight-700"
+}) {
+  const Component = level > 6 || level < 1 ? "h1" : `h${level}`;
+  const className = `Title Title__level${level} ${colorClass} ${weightClass}`;
+
+  return <Component className={className}>{children}</Component>;
+}
+
+Title.protoTypes = {
+  children: PropTypes.node.isRequired,
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6, "1", "2", "3", "4", "5", "6"]),
+  colorClass: PropTypes.strings,
+  weightClass: PropTypes.strings
+};
+
+Title.defaultProps = {
+  level: 1,
+  colorClass: "color-yellow",
+  weightClass: "font-weight-700"
+};
+
+export default Title;
